e6bede1100d655fdf48eb389de8c168433ee770c
vinaypugal <vinay.chennai17@gmail.com>
2021-05-27 23:44:25 +0530

    cleaned js and css vedor files

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,8 @@ import { Router } from "next/router";
 import { useState } from "react";
 import { RecoilRoot } from "recoil";
 import Loading from "../components/Loading";
-import "../styles/animate.css/animate.min.css";
-import "../styles/aos/aos.css";
-import "../styles/bootstrap-icons/bootstrap-icons.css";
-import "../styles/bootstrap/css/bootstrap.min.css";
-import "../styles/boxicons/css/boxicons.min.css";
 import "../styles/opensans.css";
-import "../styles/remixicon/remixicon.css";
 import "../styles/style.css";
-import "../styles/swiper/swiper-bundle.min.css";
 
 function MyApp({ Component, pageProps }) {
   const [loading, setLoading] = useState(false);
--- pages/_document.js
+++ pages/_document.js
@@ -10,17 +10,26 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" rel="stylesheet" />
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css" rel="stylesheet" />
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.5.0/font/bootstrap-icons.min.css" rel="stylesheet" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous"></link>
+        <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'/>
+        <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet"/>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/6.5.8/swiper-bundle.css" rel="stylesheet"/>
         <link href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.min.css" rel="stylesheet"></link>
+        <link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+    rel="stylesheet"/>
         <meta name="theme-color" content="#4FB2BF" />
         </Head>
         <body>
           <Main />
           <NextScript />
-          <script src="assets/vendor/aos/aos.js"></script>
-          <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-          {/* <script src="assets/vendor/php-email-form/validate.js"></script> */}
-          <script src="assets/vendor/purecounter/purecounter.js"></script>
-          <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+          <script src = "https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"/>
+          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"/>
+          <script src="https://cdn.jsdelivr.net/npm/@srexi/purecounterjs/dist/purecounter_vanilla.js"></script>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/6.5.8/swiper-bundle.min.js" />
           <script src="assets/js/main.js"></script>
         </body>
       </Html>
