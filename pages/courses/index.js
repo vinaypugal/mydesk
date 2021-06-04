2beb50f495f0f12108460eeda0378d84bc68ec70
vinaypugal <vinay.chennai17@gmail.com>
2021-06-04 19:34:01 +0530

    changed css

--- pages/courses/index.js
+++ pages/courses/index.js
@@ -38,27 +38,15 @@ const Courses = () => {
                     <div className="d-flex justify-content-between align-items-center mb-3">
                       <h3>Class 9</h3>
                     </div>
-                    <Link href="/courses/subjects">
+                    <Link href="/courses/subjects/9/stateboard">
                       <a>
                         View More
                         <i className="bx bx-link" />
                       </a>
                     </Link>
-                    <br />
-                    <br />
-                    <p>
-                      Et architecto provident deleniti facere repellat nobis
-                      iste. Id facere quia quae dolores dolorem tempore. Et
-                      architecto provident deleniti facere repellat nobis iste.
-                      Id facere quia quae dolores dolorem tempore. Et architecto
-                      provident deleniti facere repellat nobis iste. Id facere
-                      quia quae dolores dolorem tempore. Et architecto provident
-                      deleniti facere repellat nobis iste. Id facere quia quae
-                      dolores dolorem tempore.
-                    </p>
                   </div>
                 </div>
-              </div>{" "}
+              </div>
               {/* End Course Item*/}
               <div className="col-lg-3 col-md-3 d-flex align-items-stretch mt-4 mt-md-0">
                 <div className="course-item">
@@ -72,24 +60,15 @@ const Courses = () => {
                     <div className="d-flex justify-content-between align-items-center mb-3">
                       <h3>Class 10</h3>
                     </div>
-                    <a href="10th-state.html">
-                      View More <i className="bx bx-link" />
-                    </a>{" "}
-                    <br />
-                    <br />
-                    <p>
-                      Et architecto provident deleniti facere repellat nobis
-                      iste. Id facere quia quae dolores dolorem tempore. Et
-                      architecto provident deleniti facere repellat nobis iste.
-                      Id facere quia quae dolores dolorem tempore. Et architecto
-                      provident deleniti facere repellat nobis iste. Id facere
-                      quia quae dolores dolorem tempore. Et architecto provident
-                      deleniti facere repellat nobis iste. Id facere quia quae
-                      dolores dolorem tempore.
-                    </p>
+                    <Link href="/courses/subjects/10/stateboard">
+                      <a>
+                        View More
+                        <i className="bx bx-link" />
+                      </a>
+                    </Link>
                   </div>
                 </div>
-              </div>{" "}
+              </div>
               {/* End Course Item*/}
               <div className="col-lg-3 col-md-3 d-flex align-items-stretch mt-4 mt-lg-0">
                 <div className="course-item">
@@ -103,24 +82,15 @@ const Courses = () => {
                     <div className="d-flex justify-content-between align-items-center mb-3">
                       <h3>Class 11</h3>
                     </div>
-                    <a href="11th-state.html">
-                      View More <i className="bx bx-link" />
-                    </a>{" "}
-                    <br />
-                    <br />
-                    <p>
-                      Et architecto provident deleniti facere repellat nobis
-                      iste. Id facere quia quae dolores dolorem tempore. Et
-                      architecto provident deleniti facere repellat nobis iste.
-                      Id facere quia quae dolores dolorem tempore. Et architecto
-                      provident deleniti facere repellat nobis iste. Id facere
-                      quia quae dolores dolorem tempore. Et architecto provident
-                      deleniti facere repellat nobis iste. Id facere quia quae
-                      dolores dolorem tempore.
-                    </p>
+                    <Link href="/courses/subjects/11/stateboard">
+                      <a>
+                        View More
+                        <i className="bx bx-link" />
+                      </a>
+                    </Link>
                   </div>
                 </div>
-              </div>{" "}
+              </div>
               {/* End Course Item*/}
               <div className="col-lg-3 col-md-3 d-flex align-items-stretch mt-4 mt-lg-0">
                 <div className="course-item">
@@ -134,24 +104,15 @@ const Courses = () => {
                     <div className="d-flex justify-content-between align-items-center mb-3">
                       <h3>Class 12</h3>
                     </div>
-                    <a href="12th-state.html">
-                      View More <i className="bx bx-link" />
-                    </a>{" "}
-                    <br />
-                    <br />
-                    <p>
-                      Et architecto provident deleniti facere repellat nobis
-                      iste. Id facere quia quae dolores dolorem tempore. Et
-                      architecto provident deleniti facere repellat nobis iste.
-                      Id facere quia quae dolores dolorem tempore. Et architecto
-                      provident deleniti facere repellat nobis iste. Id facere
-                      quia quae dolores dolorem tempore. Et architecto provident
-                      deleniti facere repellat nobis iste. Id facere quia quae
-                      dolores dolorem tempore.
-                    </p>
+                    <Link href="/courses/subjects/12/stateboard">
+                      <a>
+                        View More
+                        <i className="bx bx-link" />
+                      </a>
+                    </Link>
                   </div>
                 </div>
-              </div>{" "}
+              </div>
               {/* End Course Item*/}
             </div>
           </div>
@@ -172,26 +133,14 @@ const Courses = () => {
                     <div className="d-flex justify-content-between align-items-center mb-3">
                       <h3>Class 9</h3>
                     </div>
-                    <Link href="/courses/cbse9th">
+                    <Link href="/courses/subjects/9/cbse">
                       <a>
                         View More <i className="bx bx-link" />
                       </a>
                     </Link>
-                    <br />
-                    <br />
-                    <p>
-                      Et architecto provident deleniti facere repellat nobis
-                      iste. Id facere quia quae dolores dolorem tempore. Et
-                      architecto provident deleniti facere repellat nobis iste.
-                      Id facere quia quae dolores dolorem tempore. Et architecto
-                      provident deleniti facere repellat nobis iste. Id facere
-                      quia quae dolores dolorem tempore. Et architecto provident
-                      deleniti facere repellat nobis iste. Id facere quia quae
-                      dolores dolorem tempore.
-                    </p>
                   </div>
                 </div>
-              </div>{" "}
+              </div>
               {/* End Course Item*/}
               <div className="col-lg-3 col-md-3 d-flex align-items-stretch mt-4 mt-md-0">
                 <div className="course-item">
@@ -205,24 +154,14 @@ const Courses = () => {
                     <div className="d-flex justify-content-between align-items-center mb-3">
                       <h3>Class 10</h3>
                     </div>
-                    <a href="10th-CBSE.html">
-                      View More <i className="bx bx-link" />
-                    </a>{" "}
-                    <br />
-                    <br />
-                    <p>
-                      Et architecto provident deleniti facere repellat nobis
-                      iste. Id facere quia quae dolores dolorem tempore. Et
-                      architecto provident deleniti facere repellat nobis iste.
-                      Id facere quia quae dolores dolorem tempore. Et architecto
-                      provident deleniti facere repellat nobis iste. Id facere
-                      quia quae dolores dolorem tempore. Et architecto provident
-                      deleniti facere repellat nobis iste. Id facere quia quae
-                      dolores dolorem tempore.
-                    </p>
+                    <Link href="/courses/subjects/10/cbse">
+                      <a>
+                        View More <i className="bx bx-link" />
+                      </a>
+                    </Link>
                   </div>
                 </div>
-              </div>{" "}
+              </div>
               {/* End Course Item*/}
               <div className="col-lg-3 col-md-3 d-flex align-items-stretch mt-4 mt-lg-0">
                 <div className="course-item">
@@ -236,24 +175,14 @@ const Courses = () => {
                     <div className="d-flex justify-content-between align-items-center mb-3">
                       <h3>Class 11</h3>
                     </div>
-                    <a href="11th-CBSE.html">
-                      View More <i className="bx bx-link" />
-                    </a>{" "}
-                    <br />
-                    <br />
-                    <p>
-                      Et architecto provident deleniti facere repellat nobis
-                      iste. Id facere quia quae dolores dolorem tempore. Et
-                      architecto provident deleniti facere repellat nobis iste.
-                      Id facere quia quae dolores dolorem tempore. Et architecto
-                      provident deleniti facere repellat nobis iste. Id facere
-                      quia quae dolores dolorem tempore. Et architecto provident
-                      deleniti facere repellat nobis iste. Id facere quia quae
-                      dolores dolorem tempore.
-                    </p>
+                    <Link href="/courses/subjects/11/cbse">
+                      <a>
+                        View More <i className="bx bx-link" />
+                      </a>
+                    </Link>
                   </div>
                 </div>
-              </div>{" "}
+              </div>
               {/* End Course Item*/}
               <div className="col-lg-3 col-md-3 d-flex align-items-stretch mt-4 mt-lg-0">
                 <div className="course-item">
@@ -267,21 +196,11 @@ const Courses = () => {
                     <div className="d-flex justify-content-between align-items-center mb-3">
                       <h3>Class 12</h3>
                     </div>
-                    <a href="12th-CBSE.html">
-                      View More <i className="bx bx-link" />
-                    </a>{" "}
-                    <br />
-                    <br />
-                    <p>
-                      Et architecto provident deleniti facere repellat nobis
-                      iste. Id facere quia quae dolores dolorem tempore. Et
-                      architecto provident deleniti facere repellat nobis iste.
-                      Id facere quia quae dolores dolorem tempore. Et architecto
-                      provident deleniti facere repellat nobis iste. Id facere
-                      quia quae dolores dolorem tempore. Et architecto provident
-                      deleniti facere repellat nobis iste. Id facere quia quae
-                      dolores dolorem tempore.
-                    </p>
+                    <Link href="/courses/subjects/12/cbse">
+                      <a>
+                        View More <i className="bx bx-link" />
+                      </a>
+                    </Link>
                   </div>
                 </div>
               </div>
