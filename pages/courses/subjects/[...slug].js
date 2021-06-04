af6f6eda08986767c55983e46ee48dac881d0d7e
vinaypugal <vinay.chennai17@gmail.com>
2021-06-04 20:57:24 +0530

    added ssr

--- pages/courses/subjects/[...slug].js
+++ pages/courses/subjects/[...slug].js
@@ -1,11 +1,9 @@
 import axios from "axios";
-import { useRouter } from "next/router";
-import React, { useEffect } from "react";
-import useSWR from "swr";
+import React from "react";
 import Footer from "../../../components/Footer";
 import Header from "../../../components/Header";
 
-const subject = ({data}) => {
+const subject = ({ data }) => {
   return (
     <>
       <Header />
@@ -21,32 +19,34 @@ const subject = ({data}) => {
         <section id="courses" className="courses">
           <div className="container" data-aos="fade-up">
             <div className="row" data-aos="zoom-in" data-aos-delay={100}>
-              {data ? (
-                data.map((item) => (
-                  <div className="col-lg-4 col-md-4 d-flex align-items-stretch" key={item._id}>
-                    <div className="course-item">
-                      <img
-                        style={{ width: 420, height: 200 }}
-                        src={`/assets/img/${item.name}.jpg`}
-                        className="img-fluid"
-                        alt="..."
-                      />
-                      <div className="course-content">
-                        <div className="d-flex justify-content-between align-items-center mb-3">
-                          <h3>
-                            Class {item.class} - <span style={{textTransform : "capitalize"}}>{item.name}</span> 
-                          </h3>
-                        </div>
-                        <a href="9th-state.html">
-                          View More <i className="bx bx-link" />
-                        </a>
+              {data.map((item) => (
+                <div
+                  className="col-lg-4 col-md-4 d-flex align-items-stretch"
+                  key={item._id}
+                >
+                  <div className="course-item">
+                    <img
+                      style={{ width: 420, height: 200 }}
+                      src={`/assets/img/${item.name}.jpg`}
+                      className="img-fluid"
+                      alt="..."
+                    />
+                    <div className="course-content">
+                      <div className="d-flex justify-content-between align-items-center mb-3">
+                        <h3>
+                          Class {item.class} -{" "}
+                          <span style={{ textTransform: "capitalize" }}>
+                            {item.name}
+                          </span>
+                        </h3>
                       </div>
+                      <a href="9th-state.html">
+                        View More <i className="bx bx-link" />
+                      </a>
                     </div>
                   </div>
-                ))
-              ) : (
-                <h1>Loading..</h1>
-              )}
+                </div>
+              ))}
               {/* End Course Item*/}
             </div>
           </div>
@@ -58,11 +58,20 @@ const subject = ({data}) => {
   );
 };
 
-export async function getServerSideProps({params}) {
-  const [cls , board] = params.slug
-  const res = await axios.get(`${process.env.URL}/api/subjects/${cls}/${board}`)
-  const data = await res.data
-  return { props: { data } , notFound : !data }
+export async function getServerSideProps({ params }) {
+  const [cls, board] = params.slug;
+  const res = await axios.get(
+    `${process.env.URL}/api/subjects/${cls}/${board}`
+  );
+  const data = await res.data;
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/courses",
+        permanent: false,
+      },
+    };
+  }
+  return { props: { data } };
 }
-
 export default subject;
