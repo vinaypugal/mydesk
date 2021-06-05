05837815cae829226fde74a306e9468dc6cf4a14
vinaypugal <vinay.chennai17@gmail.com>
2021-06-05 18:47:13 +0530

    added tutor

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 module.exports = {
   future: {
     webpack5: true,
-  },
-  "paths": {
-    "@/components/*": ["components/*"],
-    "@/middleware/*": ["middleware/*"],
-    "@/models/*": ["middleware/*"],
-    "@/utils/*": ["utils/*"]
   }
 };
