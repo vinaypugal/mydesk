5f2d71b8c8b91d3a190bc69acce304f82e346515
vinaypugal <vinay.chennai17@gmail.com>
2021-06-06 11:43:51 +0530

    added api

--- pages/register_tutor.js
+++ pages/register_tutor.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { useForm } from "react-hook-form";
